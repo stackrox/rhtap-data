f590d5b79547dcfc4c7cd250e3af9869fbae8155
Tom Martensen <tmartens@redhat.com>
2023-11-30 11:25:19 +0100

    update script to skip upload if no diff

--- scripts/dump-and-upload.sh
+++ scripts/dump-and-upload.sh
@@ -9,6 +9,12 @@ rhtap export application acs \
     -f rh-acs-tenant \
     -w data
 
-git add data/rh-acs-tenant.yaml
-git commit -m "$(date) export of ACS resources"
-git push
+DIFF="$(git diff main)"
+if [ -n "${DIFF}" ]; then
+    git add data/rh-acs-tenant.yaml
+    git commit -m "$(date) export of ACS resources"
+    git push
+
+else
+    echo "INFO: No diff in export noticed, skipping upload."
+fi
