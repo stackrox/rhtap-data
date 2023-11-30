b9d5c3652ee174f7ed819c6d35a3c0fc2f2aed5e
Tom Martensen <tmartens@redhat.com>
2023-11-30 11:26:24 +0100

    working, cleanup

--- scripts/dump-and-upload.sh
+++ scripts/dump-and-upload.sh
@@ -9,12 +9,11 @@ rhtap export application acs \
     -f rh-acs-tenant \
     -w data
 
-DIFF="$(git diff main)"
+DIFF="$(git diff)"
 if [ -n "${DIFF}" ]; then
     git add data/rh-acs-tenant.yaml
     git commit -m "$(date) export of ACS resources"
     git push
-
 else
-    echo "INFO: No diff in export noticed, skipping upload."
+    echo "INFO: No diff noticed in export, skipping upload."
 fi
